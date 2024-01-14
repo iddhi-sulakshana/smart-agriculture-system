b3160439ef3e659c74558c5b8719e343fe608a0f
iddhi dasanayaka <iddhidasanayaka@gmail.com>
2024-01-15 02:33:27 +0530

    Update CustomPagination component

--- frontend/src/components/common/CustomPagination.jsx
+++ frontend/src/components/common/CustomPagination.jsx
@@ -1,20 +1,67 @@
 import React from "react";
-import { Pagination } from "antd";
-import { Box } from "@mui/joy";
+import { ConfigProvider, Pagination } from "antd";
+import { Box, Typography } from "@mui/joy";
+import NavigateBeforeRoundedIcon from "@mui/icons-material/NavigateBeforeRounded";
+import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
 
 function CustomPagination() {
-    const onChange = (page) => {
-        console.log(page);
+    const onChange = (page) => {};
+    const itemRender = (current, type, originalElement) => {
+        const commonBoxStyles = {
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        };
+        if (type === "prev") {
+            return (
+                <Box sx={commonBoxStyles}>
+                    <NavigateBeforeRoundedIcon />
+                </Box>
+            );
+        }
+        if (type === "next") {
+            return (
+                <Box sx={commonBoxStyles}>
+                    <NavigateNextRoundedIcon />
+                </Box>
+            );
+        }
+        if (type === "page") {
+            return (
+                <Box sx={commonBoxStyles}>
+                    <Typography color="primary" level="body-md">
+                        {current}
+                    </Typography>
+                </Box>
+            );
+        }
+        return (
+            <Typography color="primary" level="body-md">
+                ...
+            </Typography>
+        );
     };
     return (
         <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
-            <Pagination
-                defaultCurrent={1}
-                total={500}
-                defaultPageSize={10}
-                showSizeChanger={false}
-                onChange={onChange}
-            />
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Pagination: {
+                            itemActiveBg: "transparent",
+                        },
+                    },
+                }}
+            >
+                <Pagination
+                    defaultCurrent={1}
+                    itemRender={itemRender}
+                    total={500}
+                    defaultPageSize={10}
+                    showSizeChanger={false}
+                    onChange={onChange}
+                />
+            </ConfigProvider>
         </Box>
     );
 }
