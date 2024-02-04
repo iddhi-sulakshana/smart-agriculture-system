4c3b189b07cf181a8f41f6935fbd7b8a10ff92d7
iddhi dasanayaka <iddhidasanayaka@gmail.com>
2024-02-04 23:39:49 +0530

    Added Table for insights

--- frontend/src/pages/Insights.jsx
+++ frontend/src/pages/Insights.jsx
@@ -1,4 +1,4 @@
-import { Box, Sheet, Table, Typography } from "@mui/joy";
+import { Box, Table, Typography } from "@mui/joy";
 import React from "react";
 
 function Insights() {
@@ -14,52 +14,52 @@ function Insights() {
                 },
             }}
         >
-            <Sheet
-                sx={{
-                    borderLeft: "1px solid",
-                    borderColor: "divider",
-                }}
-            >
-                <Typography level="h2" textAlign="center">
-                    Price Insights
-                </Typography>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Yesterday Price</th>
-                            <th>Today Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Tomato</td>
-                            <td>100</td>
-                            <td>120</td>
-                        </tr>
-                        <tr>
-                            <td>Onion</td>
-                            <td>50</td>
-                            <td>60</td>
-                        </tr>
-                        <tr>
-                            <td>Potato</td>
-                            <td>30</td>
-                            <td>40</td>
-                        </tr>
-                        <tr>
-                            <td>Carrot</td>
-                            <td>40</td>
-                            <td>45</td>
-                        </tr>
-                        <tr>
-                            <td>Beans</td>
-                            <td>60</td>
-                            <td>70</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </Sheet>
+            <Typography level="h2" textAlign="center" mb={2}>
+                Price Insights
+            </Typography>
+
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Yesterday Price</th>
+                        <th>Today Price</th>
+                        <th>Predicted Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tomato</td>
+                        <td>100</td>
+                        <td>120</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Onion</td>
+                        <td>50</td>
+                        <td>60</td>
+                        <td>Rs/Kg</td>
+                    </tr>
+                    <tr>
+                        <td>Potato</td>
+                        <td>30</td>
+                        <td>40</td>
+                        <td>Rs/Kg</td>
+                    </tr>
+                    <tr>
+                        <td>Carrot</td>
+                        <td>40</td>
+                        <td>45</td>
+                        <td>Rs/Kg</td>
+                    </tr>
+                    <tr>
+                        <td>Beans</td>
+                        <td>60</td>
+                        <td>70</td>
+                        <td>Rs/T</td>
+                    </tr>
+                </tbody>
+            </Table>
         </Box>
     );
 }
