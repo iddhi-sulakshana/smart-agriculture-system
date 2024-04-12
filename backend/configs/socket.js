633aa480ee171654d095530a353e7b02cdd57d1c
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-04-12 20:32:44 +0530

    Refactor socket.js to include morgan and socketAuthentication middlewares

--- backend/configs/socket.js
+++ backend/configs/socket.js
@@ -2,29 +2,30 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import Chat from "../models/chat.js";
 import { Message, validateMessage } from "../models/message.js";
+import morgan from "morgan";
+import socketAuthentication from "../middlewares/socketAuthentication.js";
+import authentication from "../middlewares/authentication.js";
+
+let io;
 
 export default function (app) {
     const server = createServer(app);
-    const io = new Server(server);
+    io = new Server(server);
+
+    io.engine.use(morgan("tiny"));
+    io.engine.use(socketAuthentication);
 
     // chat logic
-    io.on("connection", (socket) => {
-        const time = Date.now();
-        console.log("SOCKET " + socket.id + " connected ");
-
-        socket.on("chat message", (msg) => {
-            io.emit("chat message", msg);
+    io.on("connection", async (socket) => {
+        const userChats = await Chat.find({
+            participants: { $in: [socket.request.user] },
         });
-
-        socket.on("disconnect", () => {
-            const time2 = Date.now();
-            console.log(
-                "SOCKET " + socket.id + " disconnected after: ",
-                (time2 - time) / 1000,
-                "s"
-            );
+        userChats.forEach((chat) => {
+            socket.join(`chat-${chat._id}`);
         });
     });
 
     return server;
 }
+
+export { io };
