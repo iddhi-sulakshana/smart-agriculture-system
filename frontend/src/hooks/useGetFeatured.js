e3c3e6f3e3e955548fca4964e6353e4cbe6e912c
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-04-17 02:01:24 +0530

    Add missing toast dependency

--- frontend/src/hooks/useGetFeatured.js
+++ frontend/src/hooks/useGetFeatured.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { getURL } from "../Utils/Url";
+import { toast } from "react-toastify";
 
 async function getFeatured() {
     return new Promise((resolve, reject) => {
